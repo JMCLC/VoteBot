d798b7ddc2e3fb94d5f0f44fc00605d5b369bf46
Pulga <pulgapt123@gmail.com>
2021-12-18 21:48:11 +0000

    Added another server support

--- main.py
+++ main.py
@@ -12,11 +12,12 @@ bot = discord.Bot()
 config = json.load(open('config.json'))
 token = json.load(open("token.json"))
 guild = int(token['guildId'])
+guild2 = int(token['guildId2'])
 pollTime = int(config['pollTime'])
 cooldownTime = int(config['cooldown'])
 cooldowns = {}
 
-@bot.slash_command(guild_ids=[guild], default_permission=True)
+@bot.slash_command(guild_ids=[guild, guild2], default_permission=True)
 async def votekick(ctx, target: Option(str, "Enter your Target")):
     """Create a vote to kick someone from your voice channel"""
     if '!' in target:
@@ -74,7 +75,7 @@ async def votekick(ctx, target: Option(str, "Enter your Target")):
     else:
         await message.edit(embed = utils.newEmbed(message.embeds[0].title + " A Poll falhou"))
 
-@bot.slash_command(guild_ids=[guild])
+@bot.slash_command(guild_ids=[guild, guild2])
 @permissions.permission(user_id = int(token["defaultUser"]), permission = True)
 async def changeconfig(ctx, target: Option(str, "Enter your Target"), value: Option(str, "Enter your new value")):
     """Change the values saved on config.json"""
@@ -88,7 +89,7 @@ async def changeconfig(ctx, target: Option(str, "Enter your Target"), value: Opt
     else:
         await ctx.respond(embed = utils.newEmbed("Este valor não existe"))
 
-@bot.slash_command(guild_ids=[guild])
+@bot.slash_command(guild_ids=[guild, guild2])
 @permissions.permission(user_id = int(token["defaultUser"]), permission = True)
 async def dumpconfig(ctx):
     """Dumps the content of config.json as a response"""
